c4f1b669e92ac1b9644f80c46f80465152041f55
TomaszJankowiak <62778369+TomaszJankowiak@users.noreply.github.com>
2023-06-18 13:02:46 +0200

    refactoring

--- aol4_page-migration.py
+++ aol4_page-migration.py
@@ -49,72 +49,11 @@ def random_number(rozklad,n): # <1,n>
             return i+1
 
 
-######## HIPERKOSTKA 6 ########
-
-def distance_hypercube(x,y):
-    if len(x)!=6 or len(y)!=6:
-        return 'wrong size of binary number (not 6)'
-    d=0
-    for k in range(0,6):
-        if x[k]!=y[k]:
-            d+=1
-    return d
-
-def find_optimum_h(phase):
-    position_best=int_to_bin(0)
-    cost_best=127023892
-    for pos in range(0,64):
-        position=int_to_bin(pos)
-        cost=0
-        for j in range(0,len(phase)):
-            cost+=distance_hypercube(position,phase[j])
-        if cost < cost_best:
-            cost_best = cost
-            position_best = position
-    return position_best
-
-def move_to_min_hypercube(page_place,asks,D):
-    cost=0
-    phase=[]
-    for i in range(0,len(asks)):
-        cost+=distance_hypercube(page_place,asks[i])
-        phase.append(asks[i])
-        if len(phase)==D:
-            new_position=find_optimum_h(phase)
-            phase=[]
-            cost+=D*distance_hypercube(page_place,new_position)
-            page_place=new_position
-    return cost
-
-def flip_hypercube(page_place,asks,D):
-    cost=0
-    p=1/(2*D)
-    for i in range(0,len(asks)):
-        cost+=distance_hypercube(page_place,asks[i])
-        if random.random() < p:
-            cost += D*distance_hypercube(page_place,asks[i])
-            page_place = asks[i]
-    return cost
-
 def int_to_bin(x):
     x = bin(x)[2:]
     l = len(x)
     x = str(0) * (6 - l) + x
     return x
-
-nodes=[]
-for i in range(0,64):
-    nodes.append(int_to_bin(i))
-D=32
-page_place=int_to_bin(random_number('j',64)-1)
-distribution='j'
-asks=[]
-for i in range(0,1024):
-    asks.append(int_to_bin(random_number(distribution,64)-1))
-
-
-
-######## TORUS 3D ########
 
 def bin2_to_quat(x):
     if x=='00':
@@ -127,7 +66,7 @@ def bin2_to_quat(x):
         q='3'
     return q
 
-def int_to_quat(x): # only for 0 to 63
+def int_to_quat(x): # works only for 0 to 63
     q=''
     x=int_to_bin(x)
     q+=bin2_to_quat(x[:2])
@@ -135,52 +74,96 @@ def int_to_quat(x): # only for 0 to 63
     q+=bin2_to_quat(x[4:6])
     return q
 
-def distance_torus(x,y):
-    if len(x)!=3 or len(y)!=3:
-        return 'wrong size of quaternary number (not 3)'
-    d=0
-    for k in range(0,3):
-        tmp=abs(int(x[k])-int(y[k]))
-        if tmp==3:
-            tmp=1
-        d+=tmp
-    return d
+def distance(x,y,structure):
+    if structure=='hypercube':
+        if len(x)!=6 or len(y)!=6:
+            return 'wrong size of binary number (not 6)'
+        d=0
+        for k in range(0,6):
+            if x[k]!=y[k]:
+                d+=1
+        return d
+    if structure=='torus':
+        if len(x)!=3 or len(y)!=3:
+            return 'wrong size of quaternary number (not 3)'
+        d=0
+        for k in range(0,3):
+            tmp=abs(int(x[k])-int(y[k]))
+            if tmp==3:
+                tmp=1
+            d+=tmp
+        return d
+    else:
+        return 'unknown structure'
 
-def flip_torus(page_place,asks,D):
+def flip(page_place,asks,D,structure):
     cost=0
     p=1/(2*D)
     for i in range(0,len(asks)):
-        cost+=distance_torus(page_place,asks[i])
+        cost+=distance(page_place,asks[i],structure)
         if random.random() < p:
-            cost += D*distance_torus(page_place,asks[i])
+            cost += D*distance(page_place,asks[i],structure)
             page_place = asks[i]
     return cost
 
-
-def find_optimum_t(phase):
-    position_best=int_to_quat(0)
+def find_optimum(phase,structure):
+    if structure=='hypercube':
+        position_best=int_to_bin(0)
+    elif structure=='torus':
+        position_best=int_to_quat(0)
     cost_best=127023892
     for pos in range(0,64):
-        position=int_to_quat(pos)
+        if structure=='hypercube':
+            position=int_to_bin(pos)
+        elif structure=='torus':
+            position=int_to_quat(pos)
         cost=0
         for j in range(0,len(phase)):
-            cost+=distance_torus(position,phase[j])
+            cost+=distance(position,phase[j],structure)
         if cost < cost_best:
             cost_best = cost
             position_best = position
     return position_best
 
-def move_to_min_torus(page_place,asks,D):
+def move_to_min(page_place,asks,D,structure):
     cost=0
     phase=[]
     for i in range(0,len(asks)):
-        cost+=distance_torus(page_place,asks[i])
+        cost+=distance(page_place,asks[i],structure)
         phase.append(asks[i])
         if len(phase)==D:
-            new_position=find_optimum_t(phase)
+            new_position=find_optimum(phase,structure)
             phase=[]
-            cost+=D*distance_torus(page_place,new_position)
+            cost+=D*distance(page_place,new_position,structure)
             page_place=new_position
     return cost
 
+
+
+structure='h' # 'hypercube' or 'torus'
+distribution='j'
+
+if structure=='t':
+    structure='torus'
+elif structure=='h':
+    structure='hypercube'
+D=32
+nodes=[]
+asks=[]
+if structure=='hypercube':
+    for i in range(0,64):
+        nodes.append(int_to_bin(i))
+    page_place=int_to_bin(random_number('j',64)-1)
+    for i in range(0,1024):
+        asks.append(int_to_bin(random_number(distribution,64)-1))
+elif structure=='torus':
+    for i in range(0,64):
+        nodes.append(int_to_quat(i))
+    page_place=int_to_quat(random_number('j',64)-1)
+    for i in range(0,1024):
+        asks.append(int_to_quat(random_number(distribution,64)-1))
+
+
+print(flip(page_place, asks, D, structure))
+print(move_to_min(page_place, asks, D, structure))
 
